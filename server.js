4842fb17783882df147a2abc26662943eb5928ff
Saravanan <sarav.majestic@gmail.com>
2018-11-09 19:35:31 +0530

    Test commit

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // @flow
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 
 const app = express();
 const port = 3000;
-const DIST_DIR = path.join(__dirname, 'build');
+// const DIST_DIR = path.join(__dirname, 'build');
 
 // Serving the files on the dist folder
-app.use(express.static(DIST_DIR));
+// app.use(express.static(DIST_DIR));
 
-app.get('*', (req, res) => res.sendFile(path.join(DIST_DIR, 'index.html')));
+app.get('*', (req, res) => res.end('Hello world!!!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
