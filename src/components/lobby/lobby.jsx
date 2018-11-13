08c068d4617aeafacf10e040a03d3ca4d4ecbc3e
Saravanan <sarav.majestic@gmail.com>
2018-11-13 17:58:51 +0530

    Get all tables data and show user in tables

--- src/components/lobby/lobby.jsx
+++ src/components/lobby/lobby.jsx
@@ -6,7 +6,7 @@ import { Map } from 'immutable';
 import Table from './table';
 
 type Props = {
-  tables: [[Map<string, string | number | boolean>]],
+
 };
 
 /**
@@ -14,13 +14,42 @@ type Props = {
  * @class Lobby
  * @extends {React.Component}
    */
-class Lobby extends React.PureComponent<Props> {
+class Lobby extends React.Component<Props, State> {
+  /**
+   * Creates an instance of Lobby
+   * @param {object} props props for this component.
+   */
+  constructor(props) {
+    super(props);
+    this.state = { isLoading: true, tables: this.getTables() };
+  }
+  getTables = (data) => {
+    let arr = [];
+    for(let i=0;i < 8; i++){
+      if(data && data[i]){
+        arr[i] = data[i]
+      }else{
+        arr[i] = [];
+      }
+    }
+    return arr;
+  }
+  /**
+   * Called after component is mounted
+   * @returns {void}
+   */
+  componentDidMount() {
+    fetch('/api/table/all')
+      .then(response => response.json())
+      .then(data => this.setState({ tables: this.getTables(data), isLoading: false }));
+  }
+
   /**
    * Renders the component.
    * @returns {React.Component} The rendered component.
    */
   render() {
-    const { tables } = this.props;
+    const { tables } = this.state;
     return (
       <Card className="noShadow">
         <CardContent>
--- src/containers/LobbyContainer.js
+++ src/containers/LobbyContainer.js
@@ -2,22 +2,11 @@
 import { connect } from 'react-redux';
 import Lobby from '../components/lobby/lobby';
 
-const tables = [
-  [{ name: 'Saravanan', avatar: 'https://blogcdn1.secureserver.net/wp-content/uploads/2014/06/create-a-gravatar-beard.png' }, { name: 'Balachandrika', avatar: 'http://billie-scott.com/wp-content/uploads/2018/03/billie-gravatar.jpg' }, { name: 'Arjun', avatar: 'https://cdn3.tokendaily.co/user-images/4a2359b48887048317100f5e5d28d0d6.jpeg' }, { name: 'Saravanan', avatar: 'https://blogcdn1.secureserver.net/wp-content/uploads/2014/06/create-a-gravatar-beard.png' }],
-  [{ name: 'Russell', avatar: 'https://cdn3.tokendaily.co/user-images/4a2359b48887048317100f5e5d28d0d6.jpeg' }, { name: 'Angel', avatar: 'http://billie-scott.com/wp-content/uploads/2018/03/billie-gravatar.jpg' }],
-  [{ name: 'Ho', avatar: 'https://cdn3.tokendaily.co/user-images/4a2359b48887048317100f5e5d28d0d6.jpeg' }],
-  [],
-  [],
-  [],
-  [],
-  [],
-];
-
 /**
    * @param {Object} state Current app state.
    * @return {Object} The props to be transferred to this container.
    */
-const mapStateToProps = () => ({ tables });
+const mapStateToProps = () => ({ });
 
 /**
    * @param {Redux.dispatch} dispatch Dispatch function to sent an action to the Redux state reducer
